f63279960f31aa244711149773a8783186ecbba4
RSacerdote <rodrigo.sacerdote@hotmail.com>
2022-05-24 22:03:01 -0300

    First Commit

---
+++ aula1.py
@@ -0,0 +1,22 @@
+c = "string"  # string
+# lista ordenada (aka Array) (chaves sao numeros comecados em 0)
+c = ["string", 2, 3]
+# lista ordenada de duas dimensoes (aka matriz)
+c = [["string", 1, 2], ["string2", 3, 4]]
+c = (1, 2)  # tuplab
+# dicionario (n ordenado, as chaves podem ser qualquer coisa)
+d = {"tres": 3, "dois": 2}
+
+c = "10101"
+c = int(c)
+
+c = 1
+b = 2
+a = c + b
+if (a == 3):
+    a += 1
+elif (b == 2):
+    b += 2
+
+a = input("Escreva tua frase aqui:")
+print(a + ", boa frase")
